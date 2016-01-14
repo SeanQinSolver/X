77eb1e233aae0370db523927869f44e121b5a418
Seanqin86 <Sean@QinShawns-MacBook-Pro.local>
2016-01-14 16:43:03 -0500

    third

--- LintCode/lowest-common-ancestor-II.java
+++ LintCode/lowest-common-ancestor-II.java
@@ -80,3 +80,48 @@ public class Solution {
         return list;
     }
 }
+
+//Solution3
+public class Solution {
+    /**
+     * @param root: The root of the tree
+     * @param A, B: Two node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    
+        
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root,
+                                                 ParentTreeNode A,
+                                                 ParentTreeNode B) {
+        if (!isCovered(root, A) || !isCovered(root, B)) {
+            return null;
+        } else if (isCovered(A, B)) {
+            return A;
+        } else if (isCovered(B, A)) {
+            return B;
+        }
+        
+        ParentTreeNode sibling = getSibling(A);
+        ParentTreeNode parent = A.parent;
+        while (!isCovered(sibling, B)) {
+            sibling = getSibling(parent);
+            parent = parent.parent;
+        }
+        return parent;
+    }
+    
+    public boolean isCovered(ParentTreeNode node1, ParentTreeNode node2) {
+        if (node1 == null) return false;
+        if (node1 == node2) return true;
+        
+        return isCovered(node1.left, node2) || isCovered(node1.right, node2);
+    }
+    
+    public ParentTreeNode getSibling(ParentTreeNode node) {
+        if (node == null || node.parent == null) {
+            return null;
+        }
+        ParentTreeNode parent = node.parent;
+        return parent.left == node ? parent.right : parent.left;
+    }
+}
