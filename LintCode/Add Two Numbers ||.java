de062fc9ffd4784763e3e16f3e450d1110b7dd89
Seanqin86 <Sean@QinShawns-MacBook-Pro.local>
2016-01-16 16:09:40 -0500

    Jan 16

---
+++ LintCode/Add Two Numbers ||.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;      
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
+    public ListNode addLists2(ListNode l1, ListNode l2) {
+        // write your code here
+        int carry = 0;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        ListNode temp = helper(l1, l2, 0);
+        return reverse(temp);
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+    
+    public ListNode helper(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
+        }
+        int value = carry;
+        ListNode result = new ListNode(0);
+        if (l1 != null) value += l1.val;
+        if (l2 != null) value += l2.val;
+        result.val = value % 10;
+        if (l1 != null || l2 != null) {
+            ListNode more = helper(l1 == null ? null : l1.next, l2 == null ? null : l2.next, value >= 10 ? 1 : 0);
+            result.next = more;
+        }
+        return result;
+    } 
+}
---
+++ LintCode/Add Two Numbers.java
@@ -0,0 +1,34 @@
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root, the root of binary tree.
+     * @return true if it is a mirror of itself, or false.
+     */
+    public boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        } else {
+            return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+        }
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return check(root.left, root.right);
+        }
+    }
+}
