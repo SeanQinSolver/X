8e99b155c70c78823ae8e7ff691f31519226bba2
Seanqin86 <Sean@QinShawns-MacBook-Pro.local>
2016-02-02 22:41:46 -0500

    Feb 2 Qin

---
+++ LintCode/Cosine Similarity.java
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param A: An integer array.
+     * @param B: An integer array.
+     * @return: Cosine similarity.
+     */
+    public double cosineSimilarity(int[] A, int[] B) {
+        //空数组
+        if (A.length == 0 && B.length == 0) {
+            return 2.0;
+        }
+        
+        double doProduct = 0.0;
+        double normA = 0.0;
+        double normB = 0.0;
+        
+        for (int i = 0; i < A.length; i++) {
+            doProduct += A[i] * B[i];
+            normA += Math.pow(A[i], 2);
+            normB += Math.pow(B[i], 2);
+        }
+        if (normA == 0 && normB == 0) {
+            return 2.0;
+        }
+        return doProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+    }
+}
---
+++ LintCode/Implement Trie.java
@@ -0,0 +1,64 @@
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie trie = new Trie();
+ * trie.insert("lintcode");
+ * trie.search("lint"); will return false
+ * trie.startsWith("lint"); will return true
+ */
+class TrieNode {
+    // Initialize your data structure here.
+    public HashMap<Character, TrieNode> children;
+    public boolean hasWord;
+    public TrieNode() {
+        children = new HashMap<Character, TrieNode>();
+        hasWord = false;
+    }
+}
+
+public class Solution {
+    private TrieNode root;
+
+    public Solution() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode now = root;
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+            if (!now.children.containsKey(c)) {
+                now.children.put(c, new TrieNode());
+            }
+            now = now.children.get(c);
+        }
+        now.hasWord = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode now = root;
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+            if (!now.children.containsKey(c)) {
+                return false;
+            }
+            now = now.children.get(c);
+        }
+        return now.hasWord;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode now = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            Character c = prefix.charAt(i);
+            if (!now.children.containsKey(c)) {
+                return false;
+            }
+            now = now.children.get(c);
+        }
+        return true;
+    }
+}
